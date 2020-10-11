92e8631b846d98e36c7af1294e1a1f370e770e5a
kobachi <for.sistemstudy@gmail.com>
2020-10-11 20:21:48 +0900

    ヘッダー作成

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import HeaderComponent from "./components/HeaderComponent";
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent);
+
+const app = new Vue({
+    el: '#app',
+});
