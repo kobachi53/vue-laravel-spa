edffa9bc841a85916310b0575c9f336eb6b517d9
kobachi <for.sistemstudy@gmail.com>
2020-10-11 21:43:29 +0900

    タスク詳細コンポーネント実装完了

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import HeaderComponent from "./components/HeaderComponent";
 import VueRouter from 'vue-router';
 import TaskListComponent from "./components/TaskListComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
 
 require('./bootstrap');
 
@@ -13,10 +14,18 @@ Vue.component('header-component', HeaderComponent);
 const router = new VueRouter({
     mode: 'history',
     routes: [
+        // タスク一覧
         {
             path: '/tasks',
             name: 'task.list',
             component: TaskListComponent
+        },
+        // タスク詳細
+        {
+            path: '/tasks/:taskId',
+            name: 'task.show',
+            component: TaskShowComponent,
+            props: true
         },
     ]
 });
